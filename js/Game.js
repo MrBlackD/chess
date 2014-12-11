08210481992535f2daad656f18976d62928c6346
MrBlackD <blackpirates@mail.ru>
2014-12-11 16:18:22 +0300

    Empty project

---
+++ js/Game.js
@@ -0,0 +1,18 @@
+
+var game = new Phaser.Game(500, 350, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+
+function preload() {
+
+
+}
+
+function create() {
+
+
+}
+
+function update() {
+
+
+}
