5dfe3d7e9c0b9b9284ea54a65b77ee44345187b5
MrBlackD <blackpirates@mail.ru>
2014-12-20 01:13:21 +0300

    Drawing all figures

--- js/Game.js
+++ js/Game.js
@@ -40,7 +40,7 @@ function drawBoard(){
 	layer1.resizeWorld();
     for(var i=0;i<8;i++)
     	for(var j=0;j<8;j++)
-			chessboard.putTile((i+j+1)%2, i, j,layer1);
+			chessboard.putTile((i+j)%2, i, j,layer1);
 }
 
 function initFigures(){
@@ -53,33 +53,33 @@ function initFigures(){
 				y:-1,
 				side:"",
 				tile:0};
-	}/*
+	}
 	//rooks
-	map[0][0]=place("rook",0,0,"black");
-	map[0][7]=place("rook",0,7,"black");
-	map[7][0]=place("rook",7,0,"white");
-	map[7][7]=place("rook",7,7,"white");
-	//knights
-	map[0][1]=place("pawn",0,1,"black");
-	map[0][6]=place("pawn",0,6,"black");
-	map[7][1]=place("pawn",7,1,"white");
-	map[7][6]=place("pawn",7,6,"white");
-	//bishops
-	map[0][2]=place("pawn",0,2,"black");
-	map[0][5]=place("pawn",0,5,"black");
-	map[7][2]=place("pawn",7,2,"white");
-	map[7][5]=place("pawn",7,5,"white");
+	place(map[0][0],"rook",0,0,"black",4);
+	place(map[0][7],"rook",0,7,"black",4);
+	place(map[7][0],"rook",7,0,"white",5);
+	place(map[7][7],"rook",7,7,"white",5);
+	//knights,
+	place(map[0][1],"pawn",0,1,"black",6);
+	place(map[0][6],"pawn",0,6,"black",6);
+	place(map[7][1],"pawn",7,1,"white",7);
+	place(map[7][6],"pawn",7,6,"white",7);
+	//bishops,
+	place(map[0][2],"pawn",0,2,"black",8);
+	place(map[0][5],"pawn",0,5,"black",8);
+	place(map[7][2],"pawn",7,2,"white",9);
+	place(map[7][5],"pawn",7,5,"white",9);
 	//queens
-	map[0][3]=place("pawn",0,3,"black");
-	map[7][3]=place("pawn",7,3,"white");*/
+	place(map[0][3],"pawn",0,3,"black",10);
+	place(map[7][3],"pawn",7,3,"white",11);
 	//pawns
-	for(var j=0;j<n;j++){
+	for(var j=0;j<n;j++){ 
 		place(map[1][j],"pawn",1,j,"black",2);
 		place(map[6][j],"pawn",6,j,"white",3);
 	}
 	//kings
-	//map[0][4]=place("king",0,4,"black");
-	//map[n-1][4]=place("king",0,n-1,"white");
+	place(map[0][4],"king",0,4,"black",12);
+	place(map[n-1][4],"king",n-1,4,"white",13);
 }
 
 function place(map,name,x,y,side,tile){
